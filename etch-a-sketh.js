63916379d6510769900af5f679b0415d357779c2
LuxSolace <71519212+LuxSolace@users.noreply.github.com>
2022-10-22 18:53:35 +0200

    added size input to create grid & value failsafe

--- etch-a-sketh.js
+++ etch-a-sketh.js
@@ -1,29 +1,51 @@
 const canvas = document.getElementById("paint-container");
 const colorPicker = document.getElementById("color-picker")
+const gridSizeInput = document.getElementById("grid-size-input")
 const fragment = document.createDocumentFragment()
 let gridElement
 
+const actionButton = document.querySelectorAll("#button-container button")
+
 function createGrid(size) {
-    for (let n = 0; n < size * size; n++) {
-        const div = document.createElement("div")
-        fragment.appendChild(div)
+
+    if (size >= 0 && size <= 100) {
+
+        gridSizeInput.style.backgroundColor = "white"
+
+        for (let n = 0; n < size * size; n++) {
+            const div = document.createElement("div")
+            fragment.appendChild(div)
+        }
+    
+        canvas.replaceChildren(fragment)
+        canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+        canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`
+        gridElement = document.querySelectorAll("#paint-container div")
+        gridElement.forEach(div => {
+            div.addEventListener("mouseover", colorizeGridElement)
+        })
+
+    } else {
+        gridSizeInput.style.backgroundColor = "red"
     }
-
-    canvas.replaceChildren(fragment)
-    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`
-    gridElement = document.querySelectorAll("#paint-container div")
 }
-
-createGrid(2)
 
 function colorizeGridElement() {
     this.style.backgroundColor = colorPicker.value
 }
 
-gridElement.forEach(div => {
-    div.addEventListener("mouseover", colorizeGridElement)
+createGrid(12)
+
+gridSizeInput.addEventListener("input", () => {
+    createGrid(gridSizeInput.value)
 })
+
+actionButton.forEach(button => {
+    if (button.textContent !== "Clear") {
+    button.addEventListener("click", function() {
+        button.classList.toggle("pressed")
+    }) }
+});
 
 /* function changeMouseEvent() {
     gridElement.forEach(div => {
