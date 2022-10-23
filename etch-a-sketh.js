b7f35af84199d9b6542269ea10dc39f2166a0068
LuxSolace <71519212+LuxSolace@users.noreply.github.com>
2022-10-23 21:27:39 +0200

    added functionality for clear, grid and pen button

--- etch-a-sketh.js
+++ etch-a-sketh.js
@@ -1,10 +1,13 @@
 const canvas = document.getElementById("paint-container");
 const colorPicker = document.getElementById("color-picker")
 const gridSizeInput = document.getElementById("grid-size-input")
+const palette = document.querySelectorAll("#palette div")
+const actionButton = document.querySelectorAll("#button-container button")
+const toolsButton = document.querySelectorAll("#tools input")
+const penBtn = document.getElementById("penBtn")
+
 const fragment = document.createDocumentFragment()
 let gridElement
-
-const actionButton = document.querySelectorAll("#button-container button")
 
 function createGrid(size) {
 
@@ -31,7 +34,9 @@ function createGrid(size) {
 }
 
 function colorizeGridElement() {
+    if (penBtn.classList == "pressed") {
     this.style.backgroundColor = colorPicker.value
+    }    
 }
 
 createGrid(12)
@@ -40,19 +45,33 @@ gridSizeInput.addEventListener("input", () => {
     createGrid(gridSizeInput.value)
 })
 
-actionButton.forEach(button => {
-    if (button.textContent !== "Clear") {
-    button.addEventListener("click", function() {
-        button.classList.toggle("pressed")
-    }) }
-});
+function togglePressedStyle(target) {
+    target.forEach(button => {
+        if (button.textContent !== "Clear") {
+        button.addEventListener("click", function() {
+            button.classList.toggle("pressed")
+        }) }
+    });
+}
 
-/* function changeMouseEvent() {
-    gridElement.forEach(div => {
-        div.removeEventListener("click", colorizeGridElement)
-    })  
+togglePressedStyle(actionButton)
+togglePressedStyle(toolsButton)
 
+function toggleGrid() {
+    canvas.classList.toggle("grid")
+}
+
+function clearCanvas() {
     gridElement.forEach(div => {
-        div.addEventListener("mouseover", colorizeGridElement)
-    })    
-} */
+        div.style.backgroundColor = "white"
+    })
+}
+
+const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+
+ palette.forEach(div => {
+     div.addEventListener("click", () => {
+         colorPicker.value = rgb2hex(div.style.backgroundColor)
+     })
+})
+ 
